fc72904908566b1d20e09227fe1255effdc6d8ff
maababa <ujjwal1899@gmail.com>
2024-05-14 00:35:11 +0530

    logo added

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const Logo = () => {
   return (
-    <div>
+    <div className='text-3xl font-bold  bg-gradient-to-r from-indigo-400 to-cyan-400 text-transparent bg-clip-text hover:cursor-pointer'>
       <Link href={'/'}>
-      logo
+        FORMLY
       </Link>
     </div>
   )
